80aa804c381b778f1a312f56be366271c20981cb
Gaganashree2000 <gaganashree3132000@gmail.com>
2021-04-21 21:37:13 +0530

    first commit

---
+++ DemoGit/src/com/abc/demo/Lanch.java
@@ -0,0 +1,14 @@
+package com.abc.demo;
+
+public class Lanch {
+
+	public static void main(String[] args) {
+		
+			System.out.println("hello");
+
+			}
+
+		}
+
+
+	
---
+++ DemoGit/src/com/abc/demo/launch.java
@@ -0,0 +1,12 @@
+package com.abc.demo;
+
+public class launch {
+
+	public static void main(String[] args) {
+		
+			System.out.println("hello");
+
+			}
+
+		}
+
